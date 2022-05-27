09ce74f8a6481982411eed26a3dba36ac5025512
C B Dev Narayan <devcb@pg.cusat.ac.in>
2022-05-27 14:55:23 +0530

    add

--- LabCycles/Cycle 1/p9_Insertion_Sort.cpp
+++ LabCycles/Cycle 1/p9_Insertion_Sort.cpp
@@ -3,16 +3,34 @@
 
 void insertion_sort(int* DATA,int UB,int LB = 0)
 {
-    int min = DATA[0] ;
-    int ptr = 2 ;
+    int ptr;
     for(int i = LB + 1 ; i < UB ; i++ )
     {
-        while (DATA[i] > DATA[ptr]) 
+        int ptr = i - 1;
+        int temp = DATA[i];
+        while (ptr >= 0 && temp < DATA[ptr]) 
         {
-            if (DATA[ptr] < DATA[i])
-            {
-                
-            }
+            DATA[ptr+1] = DATA[ptr] ;
+            ptr-- ;
         }
+        DATA[ptr+1] = temp ;
     }
+}
+
+int main()
+{
+    int Arr[100];
+    int n;
+    cout << "Enter the array size : ";
+    cin >> n;
+    cout << "Enter the array elements :";
+    for(int i = 0 ; i < n ; i++){
+        cin >> Arr[i];
+    }
+    cout << "Before Sorting : ";
+    display(Arr,n);
+    insertion_sort(Arr,n);
+    cout << "After sorting : ";
+    display(Arr,n);
+    return 0;
 }
