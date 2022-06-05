a2e780df8932cd18ac8df19628541de3bf5a0891
C B Dev Narayan <devcb@pg.cusat.ac.in>
2022-06-05 10:51:10 +0530

    add sparse_matrices.cpp V2

--- LabCycles/Cycle 1/p14_sparse_matrices.cpp
+++ LabCycles/Cycle 1/p14_sparse_matrices.cpp
@@ -7,6 +7,7 @@ class SparseMatrix
         int NonZero;
         int** M;
     public:
+        SparseMatrix(){}
         SparseMatrix(int N);
         void Set_Sparse_Matrix();
         void display();
@@ -26,13 +27,25 @@ SparseMatrix::SparseMatrix(int N)
 
 void SparseMatrix::Set_Sparse_Matrix()
 {
-    cout << "Enter the entries : ";
+    cout << "Enter the entries : \n";
     for(int r = 0 ; r < NonZero ; r++)
     {
         for(int c = 0 ; c < 3 ; c++)
         {
             cin >> M[r][c];
         }
+    }
+}
+
+void SparseMatrix::display()
+{
+    for(int i = 0 ; i < NonZero ; i++)
+    {
+        for(int j = 0 ; j < 3 ; j++ )
+        {
+            cout << M[i][j] << " ";
+        }
+        cout << "\n";
     }
 }
 
@@ -58,7 +71,86 @@ void SparseMatrix::T(SparseMatrix &X)
     }
 }
 
+SparseMatrix operator+(SparseMatrix A,SparseMatrix B)
+{
+    //Check if the dimensions are the same
+    if (A.M[0][0] != B.M[0][0] || A.M[0][1] != B.M[0][0])
+    {
+        cout << "Error : Incompatible dimensions\n";
+        SparseMatrix C(0);
+        return C;
+    }
+    int Alen = A.NonZero;
+    int Blen = B.NonZero;
+    // Since we dont know the total nonzero
+    // entries in C , giving it its max value
+    SparseMatrix C(Alen+Blen);
+    int i,j,k = 1;
+    //Set the first row of C
+    C.M[0][0] = A.M[0][0];
+    C.M[0][1] = A.M[0][1];
+    while(i <= Alen && j <= Blen)
+    {
+        if ((A.M[i][0] > B.M[j][0]) || 
+            (A.M[i][0]==B.M[j][0] && A.M[i][1] > B.M[j][1]))
+        {
+            C.M[k][0] = B.M[j][0];
+            C.M[k][1] = B.M[j][1];
+            C.M[k][2] = B.M[j][2];
+            k++;j++;
+        }
+        else if((A.M[i][0] < B.M[j][0]) || 
+            (A.M[i][0] == B.M[j][0] && A.M[i][1] < B.M[j][1]))
+        {
+            C.M[k][0] = A.M[i][0];
+            C.M[k][1] = A.M[i][1];
+            C.M[k][2] = A.M[i][2];
+            k++;i++;
+        }
+        else
+        {
+            C.M[k][0] = A.M[i][0];
+            C.M[k][1] = A.M[i][1];
+            C.M[k][2] = A.M[j][2] + B.M[i][2] ;
+            k++;i++;j++;
+        }
+    }
+
+    while(i <= Alen)
+    {
+        C.M[k][0] = A.M[i][0];
+        C.M[k][1] = A.M[i][1];
+        C.M[k][2] = A.M[i][2];
+        k++;i++;
+    }
+    while(j <= Blen)
+    {
+        C.M[k][0] = B.M[j][0];
+        C.M[k][1] = B.M[j][1];
+        C.M[k][2] = B.M[j][2];
+        k++;j++;        
+    }
+    C.M[0][2] = k - 1;
+    C.NonZero = k - 1;
+    return C ;
+}
+
+
 int main()
 {
-    SparseMatrix
+    int len1,len2;
+    cout << "Enter the rows in sparse matrix 1 and 2: ";
+    cin >> len1 >> len2 ;
+    SparseMatrix S1(len1);
+    S1.Set_Sparse_Matrix();
+    SparseMatrix S2(len2);
+    S2.Set_Sparse_Matrix();
+    cout << "S1  : \n";
+    S1.display();
+    cout << "S2  : \n";
+    S2.display();
+    SparseMatrix S3 = S1 + S2 ;
+    S3.display();
+
+
 }
