b489bb48d6dd0526d8739a0cd4708c7fc0fbceb9
C B Dev Narayan <devcb@pg.cusat.ac.in>
2022-07-02 10:58:28 +0530

    add searches.cpp

--- LabCycles/Cycle 1/p2_Searches.cpp
+++ LabCycles/Cycle 1/p2_Searches.cpp
@@ -7,17 +7,24 @@ class Array
     int N;
     int MAXN;
     public:
-        Array(int n,int maxn = 100){
-            N = n;
+        int SearchSteps = 0;
+        Array(int maxn = 100){
             DATA = new int[100];
             MAXN = 100;
         }
+        friend void get_array(int m,Array &A);
         void set_Data(int index,int D){
             DATA[index] = D;
         }
-        friend int Linear_Search(Array&,int);
-        friend int binary_search(int*,int,int,int);
+        int Linear_Search(int);
+        int binary_search(int);
+        int ternary_search(int);
+        int interpolation_search(int);
+        int ternary_search(int);
+        int interpolation_search(int);
+        int Fibbonacci_Search(int);
         void display();
+
 };
 
 void Array::display()
@@ -30,23 +37,24 @@ void Array::display()
     cout << "}\n";
 }
 
-void get_array(int N,Array &A)
+void get_array(int m,Array &A)
 {
-    cout << "Enter " << N << " elements : \n";
+    A.N = m;
+    cout << "Enter " << A.N << " elements : \n";
     int val ;
-    for(int i = 0;i<N;i++)
+    for(int i = 0;i<A.N;i++)
     {
         cin >> val ;
         A.set_Data(i,val);
     }
 }
 
-int Linear_Search(Array &A,int ITEM)
+int Array::Linear_Search(int ITEM)
 {
     int LOC = -1;
-    for(int i=0;i<A.N;i++)
+    for(int i=0;i<N;i++)
     {
-        if (A.DATA[i]==ITEM)
+        if (DATA[i]==ITEM)
         {
             LOC = i;
         }
@@ -61,9 +69,11 @@ int Linear_Search(Array &A,int ITEM)
     }
 }
 
-
-int binary_search(int *DATA,int UB,int ITEM,int LB = 0)
+int Array::binary_search(int ITEM)
 {
+    SearchSteps = 0;
+    int UB = N;
+    int LB = 0;
     //set beg to lower bound
     int BEG = LB;
     //set end to Upper bound
@@ -71,6 +81,7 @@ int binary_search(int *DATA,int UB,int ITEM,int LB = 0)
     int MID = (BEG + END)/2;
     while(DATA[MID] != ITEM && BEG <= END)
     {
+        SearchSteps++;
         if (DATA[MID]<ITEM)
         {
             BEG = MID + 1;
@@ -91,12 +102,14 @@ int binary_search(int *DATA,int UB,int ITEM,int LB = 0)
     }   
 }
 
-int ternary_search(int* DATA,int UB,int ITEM,int LB = 0)
+int Array::ternary_search(int ITEM)
 {
-    int BEG = LB;
-    int END = UB;
+    SearchSteps = 0;
+    int BEG = 0;
+    int END = N;
     int MID1,MID2;
     while (BEG<=END){
+        SearchSteps++;
         MID1 = (END - BEG)/3 + BEG ;
         MID2 = ((2*(END - BEG))/3) + BEG ; 
         cout << "MID1 : " << MID1 << " MID2 : " << MID2 << endl;
@@ -120,36 +133,86 @@ int ternary_search(int* DATA,int UB,int ITEM,int LB = 0)
     return -1;
 }
 
+int Array::interpolation_search(int ITEM)
+{
+    SearchSteps = 0;
+    int BEG = 0 ;
+    int END = N ;
+    int POS;
+    while (BEG <= END && (ITEM >= DATA[BEG] && ITEM <= DATA[END]))
+    {
+        SearchSteps++;
+        POS = BEG + (ITEM - DATA[BEG])/(DATA[BEG] - DATA[END])*(END - BEG);
+        if (DATA[POS] == ITEM)
+        {
+            return POS;
+            break;
+        }
+        else if (DATA[POS] > ITEM)
+        {
+            END = POS - 1 ;
+            break;
+        }
+        else
+        {
+            BEG = POS + 1 ;
+        }
+    }
+    return -1 ; 
+}
+
+int Array::Fibbonacci_Search(int ITEM)
+{
+    SearchSteps = 0;
+    int UB = N;
+    int LB = 0;
+    int F2 = 0 ;
+    int F1 = 1 ;
+    int Fb = 1 ;
+    int offset = -1 ;
+    //To get the 3 fibonacci less than UB
+    while (Fb < UB)
+    {
+        F2 = F1 ;
+        F1 = Fb ;
+        Fb = F1 + F2 ;
+    }
+    while (Fb > 1)
+    {
+        SearchSteps++;
+        int i = offset + F2;
+        if (DATA[i] > ITEM)
+        {
+            F2 = F1 ;
+            F1 = Fb ;
+            Fb = F1 + F2 ;
+            offset = i ;
+        }
+        else if (DATA[i] < ITEM)
+        {
+            Fb = F2 ;
+            F1 = F1 - F2 ;
+            F2 = Fb - F1 ;
+        }
+        else
+        {
+            return i ;
+        }
+    }
+    if (F1 == 1 && DATA[offset + 1] == ITEM)
+    {
+        return offset + 1;
+    }
+    return -1 ;
+}
+
+
+
 
 int main()
 {
-    int size;
-    int Arr[100];
-    cout << "Enter the array size : ";cin >> size;
-    cout << "Enter the elements : ";
-    for(int i = 0; i <size; i++)
-    {
-        cin >> Arr[i] ;
-    }
-    Array A1(size);
-    A1.display();
-    int exit = 1;
-    while(exit != 0)
-    {
-        int Search_elem;
-        cout << "Enter the element to search : ";
-        cin >> Search_elem;
-        //cout << "Hiu\n";
-        int ret_index = Linear_Search(A1,Search_elem);
-        if (ret_index== -1)
-        {
-            cout << "Element not found\n" ;
-        }
-        else 
-        {
-            cout << "Element " << Search_elem << " found at " << ret_index << endl;
-        }
-        cout << "Enter 0 to exit : ";
-        cin >> exit;
-    }
+    Array A1(100);
+    
+    cout << "Menu : \n";
+    cout << "1. input"
 } 
