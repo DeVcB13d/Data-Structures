5a95725404ede42fd6c3315d0839b756b6d2bfe0
C B Dev Narayan <devcb@pg.cusat.ac.in>
2022-07-07 07:59:09 +0530

    Update Tree.cpp
    delete still not working

--- LabCycles/Tree.cpp
+++ LabCycles/Tree.cpp
@@ -11,27 +11,58 @@ class Node
 
 class Binary_tree
 {
-        
     public:
         Node* Root = 0;
         void set_root(Node* R){
             Root = R;
         }
-        Node* search(int ITEM);
+        Node* search(int ITEM,Node* LOCP);
         void insert(int ITEM);
+        int inorder_pred(int,Node*);
         void remove(int ITEM);
         void print(Node*);
 };
 
 void Binary_tree::print(Node* S)
 {
-    if (S  != NULL)
+    if (S != 0)
     {
         print(S->LEFT);
-        cout << S->DATA << "  ";
+        cout << S->DATA<<"    ";
         print(S->RIGHT);
-        //cout << "\n";
     }
+    return;
+}
+
+int Binary_tree::inorder_pred(int ITEM,Node* CURR)
+{
+    Node* PRE = CURR;
+    while(PRE != 0 && PRE->LEFT!=0){
+        PRE = PRE -> LEFT;
+    }
+    return PRE->DATA;
+}
+
+Node* Binary_tree::search(int ITEM,Node* LOCP)
+{
+    Node* PTR = Root;
+    while (ITEM != PTR->DATA)
+    {
+        LOCP = PTR;
+        if (ITEM < PTR->DATA){
+            if (PTR->LEFT==0){
+                return 0;
+            }
+            PTR = PTR -> LEFT;
+        }
+        else if (ITEM > PTR->DATA){
+            if(PTR->RIGHT==0){
+                return 0;
+            }
+            PTR = PTR -> RIGHT;
+        }
+    }
+    return PTR;
 }
 
 void Binary_tree::insert(int ITEM)
@@ -59,26 +90,26 @@ void Binary_tree::insert(int ITEM)
         Node* PTR = Root;
 
         // Find the location of the node to insert into
-        while(PTR->LEFT != 0 && PTR->RIGHT != 0)
+        while (true)
         {
             if (ITEM < PTR->DATA){
+                if (PTR->LEFT==0){
+                    PTR->LEFT = NEW;
+                    return;
+                }
                 PTR = PTR -> LEFT;
             }
-            else if(ITEM > PTR->DATA){
+            else if (ITEM > PTR->DATA){
+                if(PTR->RIGHT==0){
+                    PTR->RIGHT = NEW;
+                    return;
+                }
                 PTR = PTR -> RIGHT;
             }
-            else{
-                throw "ELEMENT ALREADY EXISTS";
+            else if(ITEM == PTR->DATA){
+                //throw "ELEMENT EXISTS";
+                return;
             }
-        }
-
-        // Insert NEW to the Binary_tree
-        if (PTR->DATA > ITEM)
-        {
-            PTR->LEFT = NEW;
-        }
-        else{
-            PTR->RIGHT = NEW;
         }
     }
     catch(const char* e)
@@ -87,22 +118,78 @@ void Binary_tree::insert(int ITEM)
     }
 }
 
+void Binary_tree::remove(int ITEM){
+    try
+    {
+        // Search and return the paren
+        Node* LOCP = 0;
+        Node* LOC = this->search(ITEM,LOCP);
+        if (LOC == 0){
+            throw "Element Not found";
+            return;
+        }
 
+        // When the Node to be deleted is a leaf
+        if (LOC->LEFT == 0 && LOC->RIGHT == 0){
+            if (ITEM == (LOCP->RIGHT)->DATA){
+                LOCP->RIGHT = 0;
+            }
+            else{
+                LOCP->LEFT = 0;
+            }
+            delete LOC;
+            return;
+        }
+        // When the 
+        else if (LOC->LEFT != 0){
+            Node* temp = LOC;
+            LOC = LOC -> LEFT;
+            delete temp;
+            return;
+        }
+        else if (LOC->RIGHT != 0){
+            Node* temp = LOC;
+            LOC = LOC -> RIGHT;
+            delete temp;
+            return;
+        }
+        else{
+            int P = inorder_pred(ITEM,LOC);
+            this->remove(P);
+            LOC->DATA = P;
+        }
+    }
+    catch(const char* e)
+    {
+        cout << e << '\n';
+    }
+    
+
+}
 int main()
 {
     //Node* ROOT = new Node;
     Binary_tree T1 ;
     //T1.set_root(ROOT);
-    T1.insert(8);
-    T1.insert(5);
-    T1.insert(9);
-    T1.insert(17);
-    T1.insert(13);
-    T1.insert(3);
     T1.insert(7);
-    //T1.insert(5);
-    T1.insert(2);
-    //T1.insert(-1);
+    T1.insert(10);
+    T1.insert(-2);
+    T1.insert(8);
+    T1.insert(3);
+    T1.insert(11);
+    T1.insert(-4);
+    T1.insert(4);
+    T1.insert(9);
+    T1.insert(12);
     T1.print(T1.Root);
+    for(int i = 0 ; i < 3 ; i++)
+    {
+        cout << "\ndelete : ";
+        int el;
+        cin>>el;
+        T1.remove(el);
+        cout << "\n";
+        //T1.print(T1.Root);
+    }
     return 0;
 }
